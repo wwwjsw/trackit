d001ddfbc5ec75634af7e72f8322127bffe9203f
guilherme machado <progr.guilherme@gmail.com>
2025-12-26 10:47:50 -0300

    feat: dynamically set Vite base path based on environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/trackit/',
+  base: process.env.NODE_ENV === 'production' 
+  ? '/trackit/' 
+  : '/',
   build: {
     outDir: 'dist',    
   },
