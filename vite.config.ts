bc6aa3b15a7866b2cc4534d80ff2edc7e8b34a12
guilherme machado <progr.guilherme@gmail.com>
2025-12-26 10:56:57 -0300

    chore: Simplify Vite base path configuration by removing conditional assignment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-  ? '/trackit/' 
-  : '/',
+  base: '/trackit/', 
   build: {
     outDir: 'dist',    
   },
